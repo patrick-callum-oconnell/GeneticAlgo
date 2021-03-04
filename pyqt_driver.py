bec6b8597d068930d8e680ad6ecab9d4cfe307f9
Patrick Callum O'Connell <79714639+patrick-callum-oconnell@users.noreply.github.com>
2021-03-04 18:23:07 -0500

    Create pyqt_driver.py

---
+++ pyqt_driver.py
@@ -0,0 +1,4 @@
+"""
+This file will be the new driver. The buttons and sliders in the GUI will control the old command line arguments
+that will be passed to the old driver which takes in command line arguments and runs the algorithm.
+"""
