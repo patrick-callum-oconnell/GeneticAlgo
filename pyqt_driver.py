1d51a6ccab354c3734e8be5a6f3b52bcc1b01bce
patrick-callum-oconnell <poconne2@bowdoin.edu>
2021-03-05 10:43:13 -0500

    moved venv out of main folder

--- pyqt_driver.py
+++ pyqt_driver.py
@@ -2,3 +2,13 @@
 This file will be the new driver. The buttons and sliders in the GUI will control the old command line arguments
 that will be passed to the old driver which takes in command line arguments and runs the algorithm.
 """
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.show()
+
+app.exec()
