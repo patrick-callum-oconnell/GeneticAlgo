a2fef1de37ac69f87bcc7a4108e7dbb8e790309d
patrick-callum-oconnell <poconne2@bowdoin.edu>
2021-03-11 21:15:04 -0500

    added to gui

--- pyqt_driver.py
+++ pyqt_driver.py
@@ -4,11 +4,37 @@ that will be passed to the old driver which takes in command line arguments and 
 """
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
 
 app = QApplication(sys.argv)
+app.setStyle('Fusion')
 
-window = QWidget()
+
+
+class SliderWindow(QWidget):
+
+    def __init__(self, *args, **kwargs):
+        super(SliderWindow, self).__init__(*args, **kwargs)
+
+        layout = QHBoxLayout()
+
+        self.setWindowTitle("Genetic Algorithm Visualizer 1.0")
+
+        label = QLabel("Set Population Size (range 0 to 10000):")
+        label.setAlignment(Qt.AlignCenter)
+
+        layout.addWidget(label)
+
+        pop_size = QSlider(Qt.Horizontal)
+        pop_size.setTickPosition(QSlider.TicksBelow)
+        pop_size.setTickInterval(10)
+
+        layout.addWidget(pop_size)
+
+        self.setLayout(layout)
+
+window = SliderWindow()
 window.show()
 
 app.exec()
